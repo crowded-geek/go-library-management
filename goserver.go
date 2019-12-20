a2e67d0d2858bf504a2cdad9a8a989b20b6c03d6
crowded-geek <aayushmanchaudhory@gmail.com>
2019-12-20 22:13:42 +0530

    added fake data

--- goserver.go
+++ goserver.go
@@ -4,7 +4,14 @@ import (
     "fmt"
     "log"
     "net/http"
+    "strconv"
 )
+
+var books [4]string
+var issuedBooks [2]string
+var m map[string]string
+var mostIssued string="Atlanta"
+var topTrend string="Where magic ends"
 
 func homePage(w http.ResponseWriter, r *http.Request){
     fmt.Fprintf(w, "Welcome to the HomePage!")
@@ -12,32 +19,33 @@ func homePage(w http.ResponseWriter, r *http.Request){
 }
 
 func bookAvailable(w http.ResponseWriter, r *http.Request){
-	//doSmth to get the data and display
-	fmt.Fprintf(w, "bookAvailable?")
+	params := r.URL.Query()
+        link := params.Get("book")
+	fmt.Fprintf(w, strconv.FormatBool(contains(books, link)))
 	fmt.Println("Endpoint Hit: bookAvailable")
 }
 
 func bookIssued(w http.ResponseWriter, r *http.Request){
-	//doSmth to get the data and display
-	fmt.Fprintf(w, "bookIssued?")
+	params := r.URL.Query()
+        link := params.Get("book")
+	fmt.Fprintf(w, strconv.FormatBool(containss(issuedBooks, link)))
 	fmt.Println("Endpoint Hit: bookIssued")
 }
 
 func mostIssuedBook(w http.ResponseWriter, r *http.Request){
-	//doSmth to get the data and display
-	fmt.Fprintf(w, "someRandomBookIdon'tKnowIfExists")
+	fmt.Fprintf(w, mostIssued)
 	fmt.Println("Endpoint Hit: mostIssuedBook")
 }
 
 func topTrendingBook(w http.ResponseWriter, r *http.Request){
-	//doSmth to get the data and display
-	fmt.Fprintf(w, "topTrendingBook?Idon'tKnowWhich")
+	fmt.Fprintf(w, topTrend)
 	fmt.Println("Endpoint Hit: topTrendingBook")
 }
 
 func issuedTo(w http.ResponseWriter, r *http.Request){
-	//doSmth to get the data and display
-	fmt.Fprintf(w, "book is given to user 1")
+	params := r.URL.Query()
+        link := params.Get("book")
+	fmt.Fprintf(w, m[link])
 	fmt.Println("Endpoint Hit: issuedTo")
 }
 
@@ -51,6 +59,36 @@ func handleRequests() {
     log.Fatal(http.ListenAndServe(":8282", nil))
 }
 
+func contains(arr [4]string, str string) bool {
+   for _, a := range arr {
+      if a == str {
+         return true
+      }
+   }
+   return false
+}
+
+func containss(arr [2]string, str string) bool {
+   for _, a := range arr {
+      if a == str {
+         return true
+      }
+   }
+   return false
+}
+
 func main() {
+
+    m = make(map[string]string)
+books[0]="Harry Potter Philosopher's stone"
+books[1]="Atlanta"
+books[2]="Where magic ends"
+books[3]="When i'll fight"
+issuedBooks[0]="Atlanta"
+issuedBooks[1]="Where magic ends"
+m["Atlanta"]="aayushman"
+m["Where magic ends"]="kamihe"
+m["When i'll fight"]="Nokomata"
+m["Harry Potter Philosopher's stone"]="Natamo"
     handleRequests()
 }
